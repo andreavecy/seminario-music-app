6ed8102cb395ea75c9cc6d7d42291921148e09d0
Andrea Vecino <andreavecino94@gmail.com>
2022-07-08 21:05:58 -0500

    clase 8/07/22

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup ,Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  loginForm: FormGroup;
+  validation_messages = {
+    email: [
+      { type: "require", message: "El email es obligatorio" },
+      { type: "pattern", message: "El email no es valido" }
+    ]
+  };
+
+  constructor(private formBuilder: FormBuilder) { 
+
+    this.loginForm = this.formBuilder.group({
+
+      email: new FormControl(
+        "",
+        Validators.compose([  
+          Validators.required,
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-]+$")
+        ])
+      ),
+
+      password: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6)
+        ])
+      )
+    })
+
+  }
+
+  ngOnInit() {
+  }
+
+}
